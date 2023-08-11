11686f1d03eae88d25d91ea5e5f3c6ca855f69e4
Justin Kuhn <justintkuhn@gmail.com>
2023-08-11 16:13:02 +0100

    front page

--- components/three/Experience.tsx
+++ components/three/Experience.tsx
@@ -1,5 +1,10 @@
-import { PivotControls, OrbitControls, Text3D, TransformControls, Html, Text, Float, MeshReflectorMaterial, Center, useMatcapTexture } from "@react-three/drei"
+"use client"
+
+import { PivotControls, OrbitControls, TransformControls, Html, Text, Float, MeshReflectorMaterial, Center, useMatcapTexture, Text3D } from "@react-three/drei"
+import { AmbientLightProbeProps, AmbientLightProps } from "@react-three/fiber";
 import { useRef } from "react"
+import { DoubleSide } from 'three'
+
 
 
 export default function Experience()
@@ -12,10 +17,9 @@ export default function Experience()
 
         <OrbitControls makeDefault />
 
-
         <directionalLight position={ [ 1, 2, 3 ] } intensity={ 1.5 } />
         <ambientLight intensity={ 0.9 } />
-        <Center>
+        <Center top>
             <Text3D font="./font/Dirtyline.json"
                 size={ 0.5 }
                 height={0.2}
@@ -24,9 +28,24 @@ export default function Experience()
                 bevelSize={ 0.02}
                 bevelOffset={0}
                 bevelSegments={5}
+                letterSpacing={0.13}
                 >
-                junebug codes
-                <meshMatcapMaterial matcap={matcapTexture}/>
+                jUneBuG
+                <meshMatcapMaterial matcap={matcapTexture} side={DoubleSide}/>
+            </Text3D>
+            <Text3D font="./font/Dirtyline.json"
+                size={ 0.8 }
+                height={0.3}
+                curveSegments={12}
+                bevelEnabled
+                bevelSize={ 0.02}
+                bevelOffset={0}
+                bevelSegments={5}
+                position={ [0, -1.2, 0]}
+                letterSpacing={0.1}
+                >
+                CoDeS
+                <meshMatcapMaterial matcap={matcapTexture} side={DoubleSide}/>
             </Text3D>
         </Center>
         
--- resources.md
+++ resources.md
@@ -1,5 +1,5 @@
 https://github.com/emmelleppi/matcaps
 https://gero3.github.io/facetype.js/
-
+https://market.pmnd.rs/model/left-hand-white-webxr
 
 Fly by jeremy [CC-BY] via Poly Pizza
