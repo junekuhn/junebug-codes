dd1f1a516852bbff28658dc272cd25d34c800875
Justin Kuhn <justintkuhn@gmail.com>
2024-04-14 11:56:10 +0100

    updating tina

--- tina/config.tsx
+++ tina/config.tsx
@@ -32,7 +32,7 @@ const config = defineConfig({
   schema: {
     collections: [
       {
-        label: "Blog Posts",
+        label: "Portfolio Items",
         name: "post",
         path: "content/posts",
         format: "mdx",
@@ -378,6 +378,136 @@ const config = defineConfig({
               contentBlockSchema,
               testimonialBlockSchema,
             ],
+          },
+        ],
+      },
+      {
+        label: "Articles",
+        name: "articles",
+        path: "content/articles",
+        format: "mdx",
+        ui: {
+          router: ({ document }) => {
+            return `/articles/${document._sys.filename}`;
+          },
+        },
+        fields: [
+          {
+            type: "string",
+            label: "Title",
+            name: "title",
+            isTitle: true,
+            required: true,
+          },
+          {
+            type: "image",
+            name: "heroImg",
+            label: "Hero Image",
+          },
+          {
+            type: "rich-text",
+            label: "Excerpt",
+            name: "excerpt",
+          },
+          {
+            type: "reference",
+            label: "Author",
+            name: "author",
+            collections: ["author"],
+          },
+          {
+            type: "datetime",
+            label: "Posted Date",
+            name: "date",
+            ui: {
+              dateFormat: "MMMM DD YYYY",
+              timeFormat: "hh:mm A",
+            },
+          },
+          {
+            type: "string",
+            label: "Type",
+            name: "type"
+          },
+          {
+            type: "rich-text",
+            label: "Body",
+            name: "_body",
+            templates: [
+              {
+                name: "DateTime",
+                label: "Date & Time",
+                inline: true,
+                fields: [
+                  {
+                    name: "format",
+                    label: "Format",
+                    type: "string",
+                    options: ["utc", "iso", "local"],
+                  },
+                ],
+              },
+              {
+                name: "BlockQuote",
+                label: "Block Quote",
+                fields: [
+                  {
+                    name: "children",
+                    label: "Quote",
+                    type: "rich-text",
+                  },
+                  {
+                    name: "authorName",
+                    label: "Author",
+                    type: "string",
+                  },
+                ],
+              },
+              {
+                name: "NewsletterSignup",
+                label: "Newsletter Sign Up",
+                fields: [
+                  {
+                    name: "children",
+                    label: "CTA",
+                    type: "rich-text",
+                  },
+                  {
+                    name: "placeholder",
+                    label: "Placeholder",
+                    type: "string",
+                  },
+                  {
+                    name: "buttonText",
+                    label: "Button Text",
+                    type: "string",
+                  },
+                  {
+                    name: "disclaimer",
+                    label: "Disclaimer",
+                    type: "rich-text",
+                  },
+                ],
+                ui: {
+                  defaultItem: {
+                    placeholder: "Enter your email",
+                    buttonText: "Notify Me",
+                  },
+                },
+              },
+              {
+                name: "VideoPlayer",
+                label: "VideoPlayer",
+                fields: [
+                  {
+                    name: "url",
+                    label: "Video URL",
+                    type: "string",
+                  },
+                ],
+              },
+            ],
+            isBody: true,
           },
         ],
       },
