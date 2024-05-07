b08216f61e7c3bf46c56df60b74ee44138fa6f80
Justin Kuhn <justintkuhn@gmail.com>
2024-05-07 16:07:49 +0100

    font fix

--- components/layout/layout.tsx
+++ components/layout/layout.tsx
@@ -20,6 +20,11 @@ export const Layout = ({
       <Head>
         <title>Junebug Codes</title>
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <>
+            <link rel="preconnect" href="https://fonts.googleapis.com" />
+            <link rel="preconnect" href="https://fonts.gstatic.com" />
+            <link href="https://fonts.googleapis.com/css2?family=Azeret+Mono:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet"/>
+          </>
         {data.theme.font === "nunito" && (
           <>
             <link rel="preconnect" href="https://fonts.googleapis.com" />
