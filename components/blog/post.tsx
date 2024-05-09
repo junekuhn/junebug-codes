4ed4dadb667f6c1006dc7b20181bc2d1901b94ff
Justin Kuhn <justintkuhn@gmail.com>
2024-05-09 15:37:38 +0100

    first post done

--- components/blog/post.tsx
+++ components/blog/post.tsx
@@ -149,7 +149,7 @@ export const Post = (props: ArticleType) => {
           className={`w-full relative	mb-8 text-2xl lg:text-6xl font-extrabold tracking-normal text-center title-font`}
         >
           <span
-            className={`bg-clip-text text-transparent bg-gradient-to-r ${
+            className={`bg-clip-text text-black bg-gradient-to-r ${
               titleColorClasses[theme.color]
             }`}
           >
---
+++ content/pages/contact.md
@@ -0,0 +1,17 @@
+---
+title: Contact
+blocks:
+  - body: >
+      # Contact
+
+
+      june [at] junebug.codes
+
+
+      ![](/uploads/DSC05797.webp)
+
+      
+    color: default
+    _template: content
+---
+
